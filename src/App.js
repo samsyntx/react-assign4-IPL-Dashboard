d814050c3d8fe6337b1762728da568c1a7188abf
Shubham <samaroraytb@gmail.com>
2023-03-14 13:12:41 +0000

    React IPL Dashboard

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 
 import Home from './components/Home'
 import NotFound from './components/NotFound'
@@ -7,13 +7,11 @@ import TeamMatches from './components/TeamMatches'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/team-matches/:id" component={TeamMatches} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App
